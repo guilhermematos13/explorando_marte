91751d99f9ea254ca9363c9ebe982682f9c612a9
guilhermematos13 <guilhermemoliveira1998@gmail.com>
2021-11-21 13:15:41 -0300

    create methods for new probe

--- src/classes/Highland.java
+++ src/classes/Highland.java
@@ -1,8 +1,10 @@
 package classes;
+import java.util.ArrayList;
 
 public class Highland {
 	private int rows;
 	private int columns;
+	private ArrayList<Probe> probes = new ArrayList();
 	
 	// Getters and Setters
 	public int getRows() {
@@ -22,11 +24,27 @@ public class Highland {
 	}
 	
 	//Constructor
-		public Highland(int rows, int columns) {
-			super();
-			this.rows = rows;
-			this.columns = columns;
-		}
+	public Highland(int rows, int columns) {
+		super();
+		this.rows = rows;
+		this.columns = columns;
+	}
+	
+	//Create probes
+	public void createProbes(Probe probe) {
+		this.probes.add(probe);
+	}
+	
+	//Create moving probe
+	public Probe moveProbes(Position startPosition, String[] moveInstruction) {
+		return null;
+		
+	}
+	
+	//Return probe
+	public ArrayList<Probe> returnProbe() {
+		return probes;
+	}
 
 	// Return in view
 	@Override
